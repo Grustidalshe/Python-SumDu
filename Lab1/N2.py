1665185f117e4b74000748247058c41c9c926ecb
Vladyslava Trubina <111081934+Grustidalshe@users.noreply.github.com>
2025-11-11 00:57:50 +0200

    add Lab1 finish

--- Lab1/N2.py
+++ Lab1/N2.py
@@ -1,13 +1,27 @@
 # Знайти та роздрукувати ряд Фібоначчі з 5-го по 25-й елемент та підрахувати їх кількість
 
-fib = [1, 1]
+fib = [0, 1]
 while len(fib) < 25:
-    fib.append(fib[-1] + fib[-2]) # fib[-1] — останній елемент списку 
-                                  # fib[-2] — передостанній елемент
+    last_value = fib[-1] # fib[-1] — останній елемент списку
+    before_last_value = fib[-2] # fib[-2] — передостанній елемент
 
-fibs = fib[4:25]  # 5-й - 25-й (індекс 4 - 24) 25 не потрапляє в діапазон
-for idx, val in enumerate(fibs, start=5):
-    print(f"{val}")
+    next_value = last_value + before_last_value
+    fib.append(next_value) 
+                           
+"""
+0 1
+0 1 1
+0 1 1 2
+0 1 1 2 3 -> 5 елемент, iндекс 4
+0 1 1 2 3 5
+0 1 1 2 3 5 8
+"""
 
+fibs = fib[4:]  # 5-й - 25-й (індекс 4 - 24) 25 не потрапляє в діапазон
+print("Ряд Фібоначчі з 5-го по 25-й елемент:")
+for number in fibs:
+    print(number)
+# for i in range(len(fibs)):
+#     print(fibs[i])
 print("Кількість чисел:", len(fibs))
 
